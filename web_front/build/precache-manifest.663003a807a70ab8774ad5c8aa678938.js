b6e050dd07c1ec4ecfe1f354a93b41ed65d3609e
david <you@example.com>
2019-08-11 22:55:49 -0500

    got lazy so just added everythin

---
+++ web_front/build/precache-manifest.663003a807a70ab8774ad5c8aa678938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c04d5f2aac60d4b0e9f21c1d087f48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ae356e5e3a3a8cccf5",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "97ae356e5e3a3a8cccf5",
+    "url": "/static/js/main.cbb49d28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
